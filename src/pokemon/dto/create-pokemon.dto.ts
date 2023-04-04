63f785339f26e32bcf2f5859e8f499c2e52f35c9
Alejo Boga <alejoboga20@gmail.com>
2023-04-04 12:27:23 -0400

    add validation for create dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePokemonDto {}
+import {
+  IsInt,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
