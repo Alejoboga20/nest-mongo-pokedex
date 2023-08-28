8134bb1b23cfad0abbf2229464d60c207a356693
Alejo Boga <alejoboga20@gmail.com>
2023-08-28 14:41:51 -0300

    create resource and use globar prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+enum Versions {
+  V1 = 'v1',
+  V2 = 'v2',
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(`api/${Versions.V1}`);
+
   await app.listen(3000);
 }
 bootstrap();
