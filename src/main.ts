4fb2e024e807e197d06108343928828e68e36f61
Alejo Boga <alejoboga20@gmail.com>
2023-08-29 14:28:39 -0300

    config global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 enum Versions {
@@ -9,6 +10,12 @@ enum Versions {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api/${Versions.V1}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
