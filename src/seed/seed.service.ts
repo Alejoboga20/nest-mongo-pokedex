7e3a64e8fd158dc2a89dab400be825a55b5eb02a
Alejo Boga <alejoboga20@gmail.com>
2023-08-30 11:47:45 -0300

    get data with axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeAPIResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
   async seedDB() {
-    return 'This action adds a new seed';
+    const { data } = await this.axios.get<PokeAPIResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      console.log({ name, no });
+    });
+
+    return data;
   }
 }
